0eabd43aa6e5e0df6d7be2b8f377367368954258
jianghaifeng@zhonglian.com <jianghaifeng@zhonglian.com>
2023-10-16 17:45:11 +0800

    更新

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
       ],
       imports: ['vue', 'vue-router'],
       dirs:[
-        'src/resources/hooks'
+        'src/resources/custom-apis'
       ]
       // 可以选择auto-import.d.ts生成的位置，使用ts建议设置为'src/auto-import.d.ts'
       // dts: 'src/auto-import.d.ts'
